2a1f427284cda57abade68e7a10dcf8f3f69d8ef
Marvin Knoll <knollmarvin6@gmail.com>
2021-02-17 17:07:46 +0100

    completed knights exercise

--- 1-knights/puzzle.py
+++ 1-knights/puzzle.py
@@ -12,21 +12,63 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # A person can't be a Knave and a Knight at the same time
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))), # XOR
+
+    Biconditional(AKnight, And(AKnight, AKnave)),
+
+    # We can also use one or both of the following statements
+    # Implication(AKnight, And(AKnight, AKnave)),  # Knights say the truth
+    # Implication(AKnave, Not(And(AKnight, AKnave)))  # Knaves lie
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # A person can't be a Knave and a Knight at the same time
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))), # XOR
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))), # XOR
+
+    Biconditional(AKnight, And(AKnave, BKnave)),
+
+    # Without Biconditional
+    # Implication(AKnight, And(AKnave, BKnave)),
+    # Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+
+ABKnight = And(AKnight, BKnight)
+ABKnave = And(AKnave, BKnave)
+
 knowledge2 = And(
-    # TODO
+    # A person can't be a Knave and a Knight at the same time
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))), # XOR
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))), # XOR
+
+    Biconditional(AKnight, Or(ABKnight, ABKnave)),  # A is a Knight if and only if Both are Knights or if Both are Knaves
+    Biconditional(BKnight, Or( # B is a Knight if and only if they are the different from eachother
+            And(AKnight, Not(BKnight)),
+            And(AKnave, Not(BKnave))
+        )
+    )
+
+    # Without Biconditionals:
+
+    # Implication(AKnight, Or(ABKnight, ABKnave)), # A is a Knight if Either A and B are Knights, or A and B are Knaves
+    # Implication(AKnave, Or( # A is a Knave if They are different
+    #         And(AKnight, Not(BKnight)),
+    #         And(AKnave, Not(BKnave))
+    #     )),
+
+    # Implication(BKnight,  Or( # B is a Knight if they are the different
+    #         And(AKnight, Not(BKnight)),
+    #         And(AKnave, Not(BKnave))
+    #     )
+    # ),
 )
 
 # Puzzle 3
@@ -35,7 +77,19 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # A person can't be a Knave and a Knight at the same time
+    And(Or(AKnight, AKnave), Not(And(AKnight, AKnave))), # XOR
+    And(Or(BKnight, BKnave), Not(And(BKnight, BKnave))), # XOR
+    And(Or(CKnight, CKnave), Not(And(CKnight, CKnave))), # XOR
+
+    # We don't know more about A
+
+    # B says
+    Implication(BKnight, AKnight),
+    Biconditional(BKnight, CKnave),
+
+    # C says
+    Biconditional(CKnight, AKnight)
 )
 
 
